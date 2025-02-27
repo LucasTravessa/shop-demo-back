49c91c6dcc913e8f2fd65badbf3bc60015930a2c
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-02-27 10:13:43 -0300

    feat: new logger middleware

--- src/_middlewares/logger.middleware.ts
+++ src/_middlewares/logger.middleware.ts
@@ -3,9 +3,32 @@ import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(LoggerMiddleware.name);
+
+  injectLogging(request: Request, response: Response): void {
+    const { method, originalUrl: url, body } = request;
+
+    const requestStart = Date.now();
+    const requestId = crypto.randomUUID();
+
+    this.logger.verbose({
+      message: `${requestId} Request to ${method} ${url}`,
+      data: body,
+    });
+
+    response.setHeader('X-Request-Id', requestId);
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const processingTime = Date.now() - requestStart;
+      const logLevel = statusCode === 404 ? 'verbose' : 'log';
+      this.logger[logLevel](
+        `${requestId} ${statusCode} response to ${method} ${url} in ${processingTime}ms`,
+      );
+    });
+  }
+
   use(req: Request, res: Response, next: NextFunction): void {
-    // Gets the request log
-    Logger.log(`Request: ${req.method} ${req.url}`);
+    this.injectLogging(req, res);
     next();
   }
 }
